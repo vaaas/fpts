0e5ea28d3376fecf5b90ef441a02cbd18565c1ca
Vasileios Pasialiokis <whiterocket@outlook.com>
2022-08-19 15:14:26 +0300

    add sleep

--- dist/promise.js
+++ dist/promise.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.next_tick = exports.next_frame = void 0;
+exports.sleep = exports.next_tick = exports.next_frame = void 0;
 function next_frame() {
     return new Promise((yes) => {
         requestAnimationFrame(() => yes());
@@ -11,3 +11,7 @@ function next_tick() {
     return new Promise((yes) => yes());
 }
 exports.next_tick = next_tick;
+function sleep(n) {
+    return new Promise(yes => setTimeout(() => yes(), n));
+}
+exports.sleep = sleep;
--- promise.ts
+++ promise.ts
@@ -1,9 +1,13 @@
 export function next_frame(): Promise<void> {
     return new Promise((yes) => {
         requestAnimationFrame(() => yes())
-    });
+    })
 }
 
 export function next_tick(): Promise<void> {
-    return new Promise((yes) => yes());
+    return new Promise((yes) => yes())
+}
+
+export function sleep(n: number): Promise<void> {
+    return new Promise(yes => setTimeout(() => yes(), n))
 }
