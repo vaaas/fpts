7f21b56ed6e77e8bb7d4d6c6983b8d80972b097e
Vasileios Pasialiokis <whiterocket@outlook.com>
2022-09-01 10:23:13 +0300

    add string/startswith

--- dist/string.js
+++ dist/string.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.str = exports.concatWith = exports.concat = void 0;
+exports.startsWith = exports.str = exports.concatWith = exports.concat = void 0;
 /** join two strings into a single string */
 const concat = (a) => (b) => a + b;
 exports.concat = concat;
@@ -12,3 +12,10 @@ exports.concatWith = concatWith;
 /** turn anything into a string */
 const str = (x) => x + '';
 exports.str = str;
+/** returns true if a string starts with a prefix
+ *
+ * - `p` — the prefix
+ * - `x` — the string
+ */
+const startsWith = (p) => (x) => x.startsWith(p);
+exports.startsWith = startsWith;
--- string.ts
+++ string.ts
@@ -1,5 +1,3 @@
-import { Ternary, Unary } from './data'
-
 /** join two strings into a single string */
 export const concat = (a: string) => (b: string) => a + b
 
@@ -10,3 +8,10 @@ export const concatWith = (d: string) => (a: string) => (b: string) => a + d + b
 
 /** turn anything into a string */
 export const str = (x: any) => x+''
+
+/** returns true if a string starts with a prefix
+ *
+ * - `p` — the prefix
+ * - `x` — the string
+ */
+export const startsWith = (p: string) => (x: string) => x.startsWith(p);
