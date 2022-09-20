7c9437db5fda4fbcffb4103078ba7e7de14f1820
Vasileios Pasialiokis <whiterocket@outlook.com>
2022-09-20 16:14:08 +0300

    duad tests

--- duad.test.mjs
+++ duad.test.mjs
@@ -8,4 +8,31 @@ describe('duad', () => {
             assert.deepEqual(duad.of(1, 2), [1, 2])
         })
     })
+
+    describe('duad', () => {
+        it('should return array of 2 elements', () => {
+            assert.deepEqual(
+                duad.duad(1)(2),
+                [1, 2]
+            )
+        })
+    })
+
+    describe('prefix', () => {
+        it('should provide prefix to a value, producing a duad', () => {
+            assert.deepEqual(
+                duad.prefix(x => x.test)({ test: 'test' }),
+                ['test', { test: 'test' }]
+            )
+        })
+    })
+
+    describe('suffix', () => {
+        it('should provide prefix to a value, producing a duad', () => {
+            assert.deepEqual(
+                duad.suffix(x => x.test)({ test: 'test' }),
+                [{ test: 'test' }, 'test']
+            )
+        })
+    })
 })
