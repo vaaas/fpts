ecd149dfee5ff599e98d5e7d5ecb6a332c52654d
Vasileios Pasialiokis <whiterocket@outlook.com>
2022-09-21 16:25:14 +0300

    add more duad functions

--- duad.ts
+++ duad.ts
@@ -1,17 +1,68 @@
-import { Unary } from './data';
+import { Binary, Unary } from './data';
 
 export const of = <A, B>(a: A, b: B): [A, B] => [a, b];
 
 export const duad = <A>(a: A) => <B>(b: B): [A, B] => [a, b]
 
 export function prefix<A, B>(f: Unary<A, B>): (x: A) => [B, A] {
-    return function(x) {
-        return [f(x), x]
-    }
+	return function(x) {
+		return [f(x), x]
+	}
 }
 
 export function suffix<A, B>(f: Unary<A, B>): (x: A) => [A, B] {
-    return function(x) {
-        return [x, f(x)]
-    }
+	return function(x) {
+		return [x, f(x)]
+	}
+}
+
+/** map for duads
+ *
+ * transform a duad [A, B] into a duad [C, D] through a binary function f
+ */
+export function map<A, B, C, D>(f: Binary<A, B, [C, D]>): (x: [A, B]) => [C, D] {
+	return function(x) {
+		return f(left(x))(right(x))
+	}
+}
+
+/** map for duads for the left value */
+export function mapl<A, B, C>(f: Unary<A, C>): (x: [A, B]) => [C, B] {
+	return function(x) {
+		return [f(left(x)), right(x)]
+	}
+}
+
+/** map for duads for the right value */
+export function mapr<A, B, C>(f: Unary<B, C>): (x: [A, B]) => [A, C] {
+	return function(x) {
+		return [left(x), f(right(x))]
+	}
+}
+
+/** apply the two values of a duad to a binary function */
+export function into<A, B, C>(f: Binary<A, B, C>): (x: [A, B]) => C {
+	return function(x) {
+		return f(left(x))(right(x))
+	}
+}
+
+export function left<A, B>(x: [A, B]): A {
+	return x[0]
+}
+
+export function right<A, B>(x: [A, B]): B {
+	return x[1]
+}
+
+export function leftAs<A, B, C>(f: Unary<A, C>): (x: [A, B]) => C {
+	return function(x) {
+		return f(left(x))
+	}
+}
+
+export function rightAs<A, B, C>(f: Unary<B, C>): (x: [A, B]) => C {
+	return function(x) {
+		return f(right(x))
+	}
 }
