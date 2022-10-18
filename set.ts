3f82011f9e44645d1554b543d04e541fd61b0942
Vasileios Pasialiokis <whiterocket@outlook.com>
2022-10-18 11:23:25 +0300

    add set/map

--- set.ts
+++ set.ts
@@ -1,5 +1,14 @@
+import { Unary } from './data'
+
 export const of = <T>(x: Iterable<T>): Set<T> => new Set(x)
 
 export const inside = <T>(xs: Set<T>) => (x: T): boolean => xs.has(x)
 
 export const outside = <T>(xs: Set<T>) => (x: T): boolean => !xs.has(x)
+
+export const map = <A, B>(f: Unary<A, B>) => (xs: Set<A>): Set<B> => {
+	const n: Set<B> = new Set()
+	for (const x of xs)
+		n.add(f(x))
+	return n
+}
