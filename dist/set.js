d68a3ad51f2e976b6ef4f6223f7e729fc4232799
Vasileios Pasialiokis <whiterocket@outlook.com>
2022-10-18 11:23:30 +0300

    add set/map

--- dist/set.js
+++ dist/set.js
@@ -1,9 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.outside = exports.inside = exports.of = void 0;
+exports.map = exports.outside = exports.inside = exports.of = void 0;
 const of = (x) => new Set(x);
 exports.of = of;
 const inside = (xs) => (x) => xs.has(x);
 exports.inside = inside;
 const outside = (xs) => (x) => !xs.has(x);
 exports.outside = outside;
+const map = (f) => (xs) => {
+    const n = new Set();
+    for (const x of xs)
+        n.add(f(x));
+    return n;
+};
+exports.map = map;
