8b840a657a8e5bf400f67048a468fab15c9cbfee
Vasileios Pasialiokis <whiterocket@outlook.com>
2022-12-20 11:29:02 +0200

    $$ should return array

--- dom.ts
+++ dom.ts
@@ -1,7 +1,7 @@
 export const qs = (q: string) => (x: HTMLElement) => x.querySelector(q)
 export const qss = (q: string) => (x: HTMLElement) => x.querySelectorAll(q)
 export const $ = (q: string) => document.querySelector(q)
-export const $$ = (q: string) => document.querySelectorAll(q)
+export const $$ = (q: string) => Array.from(document.querySelectorAll(q))
 
 export const does_not_have_class = (c: string) => (x: HTMLElement) => !x.classList.contains(c)
 
