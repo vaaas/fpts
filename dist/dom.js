c3b361731e60aa2e4e6df7de1fe57bcf79750bf1
Vasileios Pasialiokis <whiterocket@outlook.com>
2022-12-20 11:25:50 +0200

    add dom/has class, dom/does_not_have_class, dom/set_class, dom/byId

--- dist/dom.js
+++ dist/dom.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.$$ = exports.$ = exports.qss = exports.qs = void 0;
+exports.byId = exports.set_class = exports.has_class = exports.does_not_have_class = exports.$$ = exports.$ = exports.qss = exports.qs = void 0;
 const qs = (q) => (x) => x.querySelector(q);
 exports.qs = qs;
 const qss = (q) => (x) => x.querySelectorAll(q);
@@ -9,3 +9,17 @@ const $ = (q) => document.querySelector(q);
 exports.$ = $;
 const $$ = (q) => document.querySelectorAll(q);
 exports.$$ = $$;
+const does_not_have_class = (c) => (x) => !x.classList.contains(c);
+exports.does_not_have_class = does_not_have_class;
+const has_class = (c) => (x) => x.classList.contains(c);
+exports.has_class = has_class;
+const set_class = (e, c, b) => {
+    if (b)
+        e.classList.add(c);
+    else
+        e.classList.remove(c);
+    return e;
+};
+exports.set_class = set_class;
+const byId = (c) => document.getElementById(c);
+exports.byId = byId;
--- dom.ts
+++ dom.ts
@@ -2,3 +2,17 @@ export const qs = (q: string) => (x: HTMLElement) => x.querySelector(q)
 export const qss = (q: string) => (x: HTMLElement) => x.querySelectorAll(q)
 export const $ = (q: string) => document.querySelector(q)
 export const $$ = (q: string) => document.querySelectorAll(q)
+
+export const does_not_have_class = (c: string) => (x: HTMLElement) => !x.classList.contains(c)
+
+export const has_class = (c: string) => (x: HTMLElement) => x.classList.contains(c)
+
+export const set_class = (e: HTMLElement, c: string, b: boolean) => {
+    if (b)
+        e.classList.add(c)
+    else
+        e.classList.remove(c)
+    return e;
+}
+
+export const byId = (c: string) => document.getElementById(c)
