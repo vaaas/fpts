15912bd6fc9bf37aa588ebae6459724f775b7a75
Vasileios Pasialiokis <whiterocket@outlook.com>
2023-10-19 10:57:15 +0300

    clean up combinator file

--- combinator.ts
+++ combinator.ts
@@ -4,39 +4,30 @@ import type { Unary, Binary } from './data'
  *
  * Pass an argument C to a unary function B. Before returning the result, pass it through a final filter, A.
  */
-export function B<B, C>(a: Unary<B, C>): <A>(b: Unary<A, B>) => (c: A) => C {
-    return function (b) {
-        return function(c) {
-            return a(b(c));
-        }
-    }
-}
+export const b =
+    <B, C>(a: Unary<B, C>) =>
+    <A>(b: Unary<A, B>) =>
+    (c: A): C =>
+    a(b(c))
 
 /** Blackbird Combinator
  *
  * Pass two arguments, C and D, to a binary function B. Before returning the result, pass it through a final filter, A.
  */
-export function B1<C, D>(a: Unary<C, D>): <A, B>(b: Binary<A, B, C>) => (c: A) => (d: B) => D {
-    return function (b) {
-        return function (c) {
-            return function (d) {
-                return a(b(c)(d))
-            }
-        }
-    }
-}
+export const B1 = <C, D>(a: Unary<C, D>) =>
+    <A, B>(b: Binary<A, B, C>) =>
+    (c: A) =>
+    (d: B): D =>
+    a(b(c)(d))
 
 /** Cardinal combinator
  *
  * Pass arguments B and C to a binary function A, in reverse (flipped).
  */
-export function C<A, B, C>(a: Binary<A, B, C>): (b: B) => (c: A) => C {
-    return function (b) {
-        return function (c) {
-            return a(c)(b)
-        }
-    }
-}
+export const C = <A, B, C>(a: Binary<A, B, C>) =>
+    (b: B) =>
+    (c: A): C =>
+    a(c)(b)
 
 /** Dovekies combinator
  *
@@ -44,17 +35,13 @@ export function C<A, B, C>(a: Binary<A, B, C>): (b: B) => (c: A) => C {
  * However, before passing them, filter D through the unary function B
  * and E through the unary function C.
  */
-export function D<A, B, C, D, E>(a: Binary<A, B, C>): (b: Unary<D, A>) => (c: Unary<E, B>) => (d: D) => (e: E) => C {
-    return function (b) {
-        return function (c) {
-            return function (d) {
-                return function (e) {
-                    return a(b(d))(c(e))
-                }
-            }
-        }
-    }
-}
+export const D =
+    <A, B, C, D, E>(a: Binary<A, B, C>) =>
+    (b: Unary<D, A>) =>
+    (c: Unary<E, B>) =>
+    (d: D) =>
+    (e: E): C =>
+    a(b(d))(c(e))
 
 /** Dove combinator
  *
@@ -63,7 +50,8 @@ export function D<A, B, C, D, E>(a: Binary<A, B, C>): (b: Unary<D, A>) => (c: Un
  *
  * in other words, applies the filter on only the second argument
  */
-export const D1 = <A, B, C>(a: Binary<A, B, C>) =>
+export const D1 =
+    <A, B, C>(a: Binary<A, B, C>) =>
     <D>(b: Unary<D, B>) =>
     (c: A) =>
     (d: D) =>
@@ -73,7 +61,8 @@ export const D1 = <A, B, C>(a: Binary<A, B, C>) =>
  *
  * like Dove, but applies a filter only on the first argument
  */
-export const D2 = <A, B, C>(a: Binary<A, B, C>) =>
+export const D2 =
+    <A, B, C>(a: Binary<A, B, C>) =>
     <D>(b: Unary<D, A>) =>
     (d: D) =>
     (c: B) =>
@@ -94,39 +83,28 @@ export const L =
  *
  * Simply returns its argument.
  */
-export function I<T>(x: T): T {
-    return x
-}
+export const I = <T>(x: T): T => x
 
 /** Kestrel combinator (true/car).
  *
  * Always returns the first argument.
  */
-export function K<T>(a: T): (b?: any[]) => T {
-    return function (b) {
-        return a
-    }
-}
+export const K = <T>(a: T) => (b?: any): T => a
 
 /** Kite combinator (false/cdr).
  *
  * Always returns the second argument.
  */
-export function KI<T>(a?: any): (y: T) => T {
-    return function (b) {
-        return b
-    }
-}
+export const KI = <T>(a?: any) => (b: T): T => b
 
 /** Thrush combinator.
  *
  * Accepts an argument for a unary function, then the function.
  */
-export function T<A, B>(x: A): (f: Unary<A, B>) => B {
-    return function (f) {
-        return f(x)
-    }
-}
+export const T =
+    <A, B>(x: A) =>
+    (f: Unary<A, B>): B =>
+    f(x)
 
 /** Starling prime combinator, AKA phoenix combinator.
  *
@@ -136,51 +114,34 @@ export function T<A, B>(x: A): (f: Unary<A, B>) => B {
  *
  * In other words, applies the same argument twice, but through different filters.
  */
-export function S<A, B, C, D>(a: Binary<A, B, C>): (b: Unary<D, A>) => (c: Unary<D, B>) => (d: D) => C {
-    return function (b) {
-        return function (c) {
-            return function (d) {
-                return a(b(d))(c(d))
-            }
-        }
-    }
-}
+export const S =
+    <A, B, C, D>(a: Binary<A, B, C>) =>
+    (b: Unary<D, A>) =>
+    (c: Unary<D, B>) =>
+    (d: D): C =>
+    a(b(d))(c(d))
 
 /** Vireo combinator AKA cons, pair.
  *
  * Accept two arguments for a binary function, then the binary function.
  */
-export function V<A, B, C>(a: A): (b: B) => (c: Binary<A, B, C>) => C {
-    return function (b) {
-        return function (c) {
-            return c(a)(b)
-        }
-    }
-}
+export const V =
+    <A, B, C>(a: A) =>
+    (b: B) =>
+    (c: Binary<A, B, C>): C =>
+    c(a)(b)
 
 /** Warbler combinator AKA elementary duplicator
  *
  * Apply argument B to binary function A twice.
  */
-export function W<A, B>(a: Binary<A, A, B>): (b: A) => B {
-    return function (b) {
-        return a(b)(b)
-    }
-}
-
-/** Uncurried implementation of Warbler, AKA elementary duplicator
- *
- * Apply argument B to binary function A twice.
- */
-export function Wu<A, B, C>(a: (a: A, b: B) => C) {
-    return function (b: A & B) {
-        return a(b, b)
-    }
-}
+export const W =
+    <A, B>(a: Binary<A, A, B>) =>
+    (b: A): B =>
+    a(b)(b)
 
 /** spread combinator */
-export function spread<A extends Array<any>, B>(f: (...xs: A) => B) {
-    return function (xs: A): B {
-        return f(...xs)
-    }
-}
+export const spread =
+    <A extends Array<any>, B>(f: (...xs: A) => B) =>
+    (xs: A): B =>
+    f(...xs)
