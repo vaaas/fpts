1e5ef980f0e97d9a1d5c36b2759b63da36fbca3d
Vasileios Pasialiokis <whiterocket@outlook.com>
2023-10-19 11:01:23 +0300

    blackbird test

--- combinator.test.ts
+++ combinator.test.ts
@@ -21,11 +21,19 @@ import {
 describe('combinator', () => {
     const str = (x: any): string => x + ''
     const inc = (x: number) => x + 1
+    const add = (x: number) => (y: number) => x + y
 
     it('should implement bluebird', () => {
         assert.equal(
             '2',
             B(str)(inc)(1)
+        )
+    })
+
+    it('should implement blackbird', () => {
+        assert.equal(
+            '3',
+            B1(str)(add)(1)(2)
         )
     })
 })
